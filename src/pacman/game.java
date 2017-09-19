d457f6be6d3f3ef3dd27a44fc6d549db2d99ee14
Nathaniel Sigafoos <nathaniel.b.sigafoos@gmail.com>
2017-09-19 16:56:10 -0400

    Set background image

--- src/pacman/game.java
+++ src/pacman/game.java
@@ -11,21 +11,13 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.Background;
 
-public class game extends Application{
+public class game extends Application {
         @Override
         public void start(Stage primaryStage){
                 Pane pane = new Pane();
-                Image img = new Image("https://cdn.goandroid.co.in/wp-content/uploads/2015/04/wpid-pacman.jpg");
-                BackgroundImage bgImg = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, 
-                                BackgroundRepeat.NO_REPEAT,BackgroundPosition.DEFAULT, new BackgroundSize(448.0, 496.0, false, false, true, false));
-                pane.setBackground(new Background(bgImg));
-                pane.setStyle("-fx-background-image: url(https://cdn.goandroid.co.in/wp-content/uploads/2015/04/wpid-pacman.jpg)");
+
+                pane.setStyle("-fx-background-image: url(File:../img/emptyMap.png)");
                 Scene scene = new Scene(pane, 448, 496);
                 primaryStage.setTitle("circles");
                 primaryStage.setScene(scene);
